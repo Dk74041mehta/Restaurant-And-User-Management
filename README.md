4530ff18e5876e7767ec7a81026bc53580a9d956
Dilip Kumar <dilipmehta70876@gmail.com>
2025-10-23 18:47:04 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://restaurant-and-user-management.onrender.com
+https://user-app-zodj.onrender.com
--- restaurant-app/src/main.jsx
+++ restaurant-app/src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/index.css'  // This imports all base styles
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
