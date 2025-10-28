39fcad33b7266975cc99384a5169f1291f08593e
Dilip Kumar <dilipmehta70876@gmail.com>
2025-10-28 22:38:43 +0530

    commit

--- restaurant-app/src/App.jsx
+++ restaurant-app/src/App.jsx
@@ -173,7 +173,7 @@ const TableCard = ({ table, onDelete }) => {
     return (
         <div className={`table-card ${cardClass}`} onClick={() => console.log(`Table ${table.id} clicked`)}>
             {/* Top Row: Delete Button */}
-            <div className="flex-justify-end w-full">
+            <div >
                 <button 
                     onClick={(e) => {
                         e.stopPropagation();
@@ -187,7 +187,7 @@ const TableCard = ({ table, onDelete }) => {
                     className={`table-card__delete-btn ${deleteIconClass} ${table.isReserved ? 'cursor-not-allowed' : ''}`}
                     title={table.isReserved ? "Reserved tables cannot be deleted" : "Delete Table"}
                 >
-                    <svg className="w-10 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-20 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
